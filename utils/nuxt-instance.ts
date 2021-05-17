b09dbcc1097c1a40393f4085022d5cd8f3ac7311
“gabrielcaiana” <gabrielcaiana@hotmail.com>
2021-05-17 20:21:17 -0300

    configurando instancia do cookies-universal

--- utils/nuxt-instance.ts
+++ utils/nuxt-instance.ts
@@ -1,9 +1,15 @@
 import { NuxtAxiosInstance } from '@nuxtjs/axios'
+import { NuxtCookies } from 'cookie-universal-nuxt'
 
 let $axios: NuxtAxiosInstance
+let $cookies: NuxtCookies
 
 export function initializeAxios(axiosInstance: NuxtAxiosInstance) {
   $axios = axiosInstance
 }
 
-export { $axios }
+export function initializeCookies(cookiesIntance: NuxtCookies) {
+  $cookies = cookiesIntance
+}
+
+export { $axios, $cookies }
