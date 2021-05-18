6af72ae16c042eb0bf72abb74bc78e16a73c406f
“gabrielcaiana” <gabrielcaiana@hotmail.com>
2021-05-17 23:38:10 -0300

    protegendo rota de dashboard

--- pages/about.vue
+++ pages/about.vue
@@ -4,6 +4,6 @@
 <script lang="ts">
 import Vue from 'vue'
 export default Vue.extend({
-  layout: 'ibook',
+  layout: 'ibook'
 })
 </script>
--- pages/dashboard.vue
+++ pages/dashboard.vue
@@ -1,5 +1,10 @@
 <template>
-    <div>
-        dashboard
-    </div>
+  <div>dashboard</div>
 </template>
+
+<script lang="ts">
+import Vue from 'vue'
+export default Vue.extend({
+  middleware: 'auth',
+})
+</script>
